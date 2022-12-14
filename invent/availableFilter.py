a624edfd84c52d4d6eb49d9e045505738f1d4a5b
nikminer <nikminerfeed@gmail.com>
2022-12-14 03:40:34 +0300

    1

--- invent/availableFilter.py
+++ invent/availableFilter.py
@@ -1,19 +1,26 @@
 from django.contrib.admin import SimpleListFilter
 
 class AvailableTechFilter(SimpleListFilter):
-    title = "Стутус оборудования"
+    # Указывает текстовое обозначение
+    title = "Статус оборудования"
+    # Параметр отвечает за определение в параметрах GET зпроса 
     parameter_name = 'employer'
    
+    # Перегружаем метод ответственный за отображение параметров фильтрации
     def lookups(self, request, model_admin):
         return (
-            ('available', 'Совободные'),
+            ('available', 'Свободные'),
             ('unavailable', 'Занятые')
         )
     
+    # Перегружаем метод ответственный за формирование запроса фильтра
     def queryset(self, request, queryset):
+        # Если не задан то возвращаем обычный запрос
         if not self.value():
             return queryset
+        # Если фильтр соответствует значению "Свободные", то фильтруем по тем записям где нет сотрудника
         if self.value().lower() == 'available':
             return queryset.filter(employer_id__isnull=True)
+        # Если фильтр соответствует значению "Занятые", то фильтруем по тем записям где есть сотрудник
         elif self.value().lower() == 'unavailable':
             return queryset.filter(employer_id__isnull=False)
